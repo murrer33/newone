ab973de96a5f0ce2b4a0c0eb75a90eb8b48e6857
murrer33 <berke.eutus@gmail.com>
2025-04-14 11:12:28 +0300

    Update googleFormsIntegration.ts

--- src/utils/googleFormsIntegration.ts
+++ src/utils/googleFormsIntegration.ts
@@ -18,7 +18,7 @@ interface WaitlistFormData {
  */
 export const submitToGoogleForm = async (
   formData: WaitlistFormData,
-  googleFormId: string = '1oKnGauCn2esBjwOqktL1bODFLy6H8kfSiUiHEh8zeTfSB66YlxZs5oUn'
+  googleFormId: string = '12vQ17UPxYvvFoCBZ4a8ynyOxzSbePMlYuZ28SG_2V4k'
 ): Promise<boolean> => {
   try {
     // Get the pre-filled form URL with our data
